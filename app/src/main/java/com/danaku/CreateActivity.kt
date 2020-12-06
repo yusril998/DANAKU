7545c5a2e3a80a45227c6612e64966484f4ef986
Nensyenlanda <nensyputri12@gmail.com>
2020-12-06 13:22:40 +0700

    Create

--- app/src/main/java/com/danaku/CreateActivity.kt
+++ app/src/main/java/com/danaku/CreateActivity.kt
@@ -27,24 +27,41 @@ class CreateActivity :AppCompatActivity(), View.OnClickListener {
         when (v.getId()){
             R.id.simpan -> {
                 val nama  = findViewById<EditText>(R.id.nama)
-                val notelp  = findViewById<EditText>(R.id.notelep)
+                val notelp  = findViewById<EditText>(R.id.notelp)
+                val kantor = findViewById<EditText>(R.id.kantor)
+                val email = findViewById<EditText>(R.id.email)
+                val alamat = findViewById<EditText>(R.id.alamat)
+                val fb = findViewById<EditText>(R.id.fb)
+                val ig = findViewById<EditText>(R.id.ig)
+                val wa = findViewById<EditText>(R.id.wa)
                 val database = FirebaseDatabase.getInstance()
 
                 val getnamalengkap: String = nama.getText().toString()
-                val getnotlp: String = notelp.getText().toString()
-
+                val getnotelp: String = notelp.getText().toString()
+                val getkantor: String =  kantor.getText().toString()
+                val getemail: String = email.getText().toString()
+                val getalamat: String = alamat.getText().toString()
+                val getfb: String = fb.getText().toString()
+                val getig: String = ig.getText().toString()
+                val getwa: String = wa.getText().toString()
                 val getReference: DatabaseReference
                 getReference = database.reference
 
-                if (isEmpty(getnamalengkap) || isEmpty(getnotlp)) {
+                if (isEmpty(getnamalengkap) || isEmpty(getnotelp)) {
                     Toast.makeText(this, "DataTidak ada yang kosong",
                         Toast.LENGTH_SHORT).show()
                 } else {
-                    getReference.child("admin").child("Daftarnama").push()
-                        .setValue(KontakModel(getnamalengkap, getnotlp))
+                    getReference.child("admin").child("Daftarkontak").push()
+                        .setValue(KontakModel(getnamalengkap, getnotelp,  getkantor, getemail, getalamat, getfb, getig, getwa))
                         .addOnCompleteListener {
                             nama.setText("")
                             notelp.setText("")
+                            kantor.setText("")
+                            email.setText("")
+                            alamat.setText("")
+                            fb.setText("")
+                            ig.setText("")
+                            wa.setText("")
                             Toast.makeText(this, " Data Tersimpan" , Toast.LENGTH_SHORT).show()
                         }
                 }
